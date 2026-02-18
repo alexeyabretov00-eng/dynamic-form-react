2a266426160c05b6f85ceee85960860ffc6a7628
Alexey Abretov <abretov@mail.ru>
2026-02-18 22:53:52 +0300

    fix: clean up webpack configuration and improve formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,68 +2,67 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
-    entry: path.resolve(__dirname, './index.tsx'),
-    module: {
-        rules: [{
-            test: /\.(js|ts|jsx|tsx)$/,
-            use: { loader: 'ts-loader' }, 
-            exclude: /node_modules/
-        },
-        {
-            test: /\.css$/,
-            use: [
-                "style-loader",
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
-                        }
-                    }
-                }
-            ]
-        }]
-    },
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js',
-        clean: true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        modules: [
-            'node_modules',
-            './src'
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: path.resolve(__dirname, './index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|jsx|tsx)$/,
+        use: { loader: 'ts-loader' },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+          },
         ],
-        alias: {
-            '@components': [path.resolve(__dirname, './src/components')],
-            '@contexts': [path.resolve(__dirname, './src/contexts')],
-            '@containers': [path.resolve(__dirname, './src/containers')],
-            '@types': [path.resolve(__dirname, './src/types/types')],
-            '@store': [path.resolve(__dirname, './src/store')],
-        }
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: ['node_modules', './src'],
+    alias: {
+      '@components': [path.resolve(__dirname, './src/components')],
+      '@contexts': [path.resolve(__dirname, './src/contexts')],
+      '@containers': [path.resolve(__dirname, './src/containers')],
+      '@types': [path.resolve(__dirname, './src/types/types')],
+      '@store': [path.resolve(__dirname, './src/store')],
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, './dist')
-        },
-        port: 4002,
-        server: {
-            type: 'https'
-        },
-        client: {
-            overlay: false
-        },
-        compress: true,
-        hot: true,
-        open: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            inject: 'body'
-        })
-    ]
-}
+    port: 4002,
+    server: {
+      type: 'https',
+    },
+    client: {
+      overlay: false,
+    },
+    compress: true,
+    hot: true,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body',
+    }),
+  ],
+};
